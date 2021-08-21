634fcc0d358e957dc221e538e2e7dfa0ff57e318
Mr. Joe <radics.pepe@gmail.com>
2021-08-21 15:26:24 +0200

    ez már kiirja az árat

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
+    let showAmount = document.querySelector("span.show-amount");
     let amount = parseInt(amountInput.value) * price;
-    alert("Fizetendő: " + amount + "Ft");
+   
+    showAmount.innerHTML = amount;
+
 }
