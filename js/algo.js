57ee579db3a0780c97e2fb0e3ebbfc791b06a2dc
Mr. Joe <radics.pepe@gmail.com>
2021-08-24 21:18:34 +0200

    mai 08.24

---
+++ js/algo.js
@@ -0,0 +1,53 @@
+/* Összegzés algoritmusa:
+
+össszeg = 0
+CIKLUS AMÍG van még szám, ADDIG
+    Szám = Következő elem
+    Összeg = összeg + szám
+CIKLUS VÉGE
+*/
+
+
+
+/*
+Számlálás algoritmusa: 
+
+db = 0
+CIKKLUS AMÍG van még szám, ADDIG
+    szám = a következő elem 
+    HA igaz a feltétel a számra, AKKOR
+        db = db+1
+    FELTÉTEL VÉGE
+CIKLUS VÉGE
+*/
+
+
+
+
+/* 
+
+Szélsőérték keresés algoritmusa: 
+
+legnagyobb = első elem
+CIKLUS AMÍG van még szám, ADDIG
+    szám = következő szám 
+    HA a szám n> legnagyobb, AKKOR
+        legnagyobb = szén 
+    FELTÉTEL VÉGE
+CIKLUS VÉGE
+*/
+
+
+
+
+/*
+Az eldöntés algoritmusa:
+
+találat = HAMIS 
+CIKLUS AMÍG van elem ÉS  NEM találat
+    szám = következő elem 
+    Ha igaz a felltétel a számra, AKKOR 
+        találat = IGAZ
+    FELTÉTEL VÉGE
+CIKLUS VÉGE
+*/
