655dc57898db301316729ce70e1771127afb2c8c
Mr. Joe <radics.pepe@gmail.com>
2021-08-26 21:33:45 +0200

    algos08.26

--- js/algo.js
+++ js/algo.js
@@ -7,6 +7,12 @@ CIKLUS AMÍG van még szám, ADDIG
 CIKLUS VÉGE
 */
 
+let numericArray = [1, 3, 2, 5, 4, 7, 6, 9];
+let sum =0;
+for (let i = 0; i< numericArray.length; i++){
+    sum += numericArray[i];
+}
+console.log("Sum: ",sum);
 
 
 /*
@@ -21,7 +27,13 @@ CIKKLUS AMÍG van még szám, ADDIG
 CIKLUS VÉGE
 */
 
-
+let db = 0;
+for (let i = 0; i< numericArray.length; i++){
+    if(numericArray[i] % 2 ==0){
+        db++;
+    }
+}
+console.log("Even numbers: ", db)
 
 
 /* 
@@ -32,10 +44,26 @@ legnagyobb = első elem
 CIKLUS AMÍG van még szám, ADDIG
     szám = következő szám 
     HA a szám n> legnagyobb, AKKOR
-        legnagyobb = szén 
+        legnagyobb = szám 
     FELTÉTEL VÉGE
 CIKLUS VÉGE
 */
+let biggest = numericArray[0];
+for (let i = 0; i < numericArray.length; i++) {
+   if (numericArray[i] > biggest) {
+        biggest = numericArray[i];
+   }
+}
+console.log("The biggest element: ", biggest);
+
+
+let smallest = numericArray[0];
+for (let i = 0; i < numericArray.length; i++) {
+   if (numericArray[i] < smallest) {
+        smallest = numericArray[i];
+   }
+}
+console.log("The biggest element: ", smallest);
 
 
 
@@ -51,3 +79,11 @@ CIKLUS AMÍG van elem ÉS  NEM találat
     FELTÉTEL VÉGE
 CIKLUS VÉGE
 */
+
+let contains = false;
+for (let i = 0; i < numericArray.length && !contains; i++) {
+    if (numericArray[i] ==5){
+        contains =true;
+    }
+}
+console.log("This array contains 5:", contains)
