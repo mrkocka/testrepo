09a2fa1a2f08504f4dd748d91e59ffbaba7b04bd
Mr. Joe <radics.pepe@gmail.com>
2021-09-03 12:12:02 +0200

    Józsi agymenes

--- js/about.js
+++ js/about.js
@@ -1,21 +1,3 @@
-/*<tr>
-    <td>1</td>
-    <td>Kiss</td>
-    <td>János</td>
-    <td>55</td>
-    <td>
-        <div class="btn-group">
-            <button class="btn-info btn">
-                <i class="fas fa-sync-alt"></i>
-            </button>
-            <button class="btn btn-danger">
-                <i class="far fa-trash-alt"></i>
-            </button>
-        </div>
-    </td>
-</tr>*/
-
-
 
 let users = [
     {surname: "Berger", firstname: "Whitney", age: 22},
@@ -35,6 +17,26 @@ let createTD = (html, parent) => {
     parent.appendChild(td);
 };
 
+let createButtonGroup = parent => {
+
+    let group = documet.createElement("div");
+    group.className = "btn-group";
+
+    let btnInfo = document.createElement("button");
+    btnInfo.className = "btn-info btn";
+    btnInfo.innerHTML = '<i class="fas fa-sync-alt"></i>';
+
+    let btnDanger = document.createElement("button");
+    btnDanger.className = "btn btn-danger";
+    btnDanger.innerHTML = '<i class="fas fa-sync-alt"></i>';
+
+    group.appendChild(btnInfo);
+    group.appendChild(btnDanger);
+
+    let td = document.createElement("td");
+    td.appendChild(group);
+    parent.appendChild(td);
+}
 
 for (let k in users) {
     let tr = document.createElement("tr");
@@ -42,5 +44,6 @@ for (let k in users) {
     for(let value of Object.values(users[k])) {
         createTD(value, tr);
     }
+    createButtonGroup(tr);
     tableBody.appendChild(tr);
 }
